2ff1e24a752321ee29d1f2ef5075ca25ae0a3563
Paul Vorbach <paul@vorba.ch>
2014-02-05 10:49:24 +0100

    Adapt test.js

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-var pandoc = require('./pandoc.js');
+var pdc = require('./pdc.js');
 
 // print markdown
-pandoc('# Heading', 'markdown', 'html', function (err, result) {
+pdc('# Heading', 'markdown', 'html', function (err, result) {
   if (err)
     throw err;
   console.log(result);
 });
 
 // show pandoc version
-pandoc('', 'markdown', 'html', [ '-v' ],
+pdc('', 'markdown', 'html', [ '-v' ],
     function (err, result) {
   if (err)
     throw err;
