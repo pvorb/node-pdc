2c5e23efe6cf4bd0fe3ae49fc2a69422e3c29e3b
goumang2010 <goumang2010@live.com>
2017-08-14 22:21:39 +0800

    docx test

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var pdc = require('./pdc.js');
 
 // print markdown
@@ -5,6 +6,13 @@ pdc('# Heading', 'markdown', 'html', function (err, result) {
   if (err)
     throw err;
   console.log(result);
+});
+
+// write docx
+pdc('# Heading docx', 'markdown', 'docx', function (err, result) {
+  if (err)
+    throw err;
+  fs.createWriteStream('test.docx').write(result);
 });
 
 // show pandoc version
